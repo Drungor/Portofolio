a8464bbf78ac386764002f11c00e827f4cda3d0c
Leopold <leopold.mtr@outlook.com>
2024-11-11 16:32:45 +0900

    chore: update React and React DOM to version 18.3.1, add react-scroll dependency, and refactor Home component to use Header

---
+++ src/app/components/resume/resume.tsx
@@ -0,0 +1,25 @@
+import react from 'react';
+
+import React from 'react';
+import { useState } from 'react';
+
+
+//function to show out optioin to download resume in fonction of the language selected
+//need to implement the modal and the dopwnload function 
+// function ShowModalForDownloadResume() {
+//     const [isVisible, setIsVisible] = useState(false);
+
+//     return (
+//         <div>
+//             <button onClick={() => setIsVisible(!isVisible)}>
+//                 {isVisible ? "hide" : "show"} Download text
+//             </button>
+//             {isVisible && <p>Download text</p>}
+//         </div>
+//     );
+// }
+
+
+const Resume = ({children,...props}) => {[
+
+]}
